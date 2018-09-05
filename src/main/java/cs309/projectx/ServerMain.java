c3f3713fa9bd504bcd0715992fc501607d77462f
maxrmedberry <31864135+maxrmedberry@users.noreply.github.com>
2018-09-05 15:31:34 -0500

    Initial commit

---
+++ src/main/java/cs309/projectx/ServerMain.java
@@ -0,0 +1,13 @@
+package cs309.projectx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class ServerMain {
+	public static void main( String[] args )
+    {
+        SpringApplication.run(ServerMain.class, args);
+    }
+
+}
